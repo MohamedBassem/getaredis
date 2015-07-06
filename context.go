298cbb08ead75771cbaadf5ed04a921731f9f40e
MohamedBassem <medoox240@gmail.com>
2015-07-06 05:59:13 +0200

    Parsing config file

--- context.go
+++ context.go
@@ -1,23 +1,52 @@
 package getaredis
 
 import (
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"time"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/fsouza/go-dockerclient"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
+
+type configuration struct {
+	Database        map[string]string `yaml:"database"`
+	DockerHost      string            `yaml:"dockerHost"`
+	MaxInstanceSize int               `yaml:"maxInstanceSize"`
+}
 
 type context struct {
 	dockerClient docker.Client
 	db           gorm.DB
+	config       configuration
 }
 
-func Init() context {
+func Init(configPath string) (*context, error) {
 	rand.Seed(time.Now().UnixNano())
-	tmp, _ := docker.NewClient("unix:///var/run/docker.sock")
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	config := new(configuration)
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+	tmp, _ := docker.NewClient(config.DockerHost)
+	databaseHost := fmt.Sprintf("%v:%v@/%v?charset=utf8&parseTime=True&loc=Local", config.Database["user"], config.Database["password"], config.Database["dbname"])
+	tmp2, err := gorm.Open("mysql", databaseHost)
+	if err != nil {
+		return nil, err
+	}
+	tmp2.AutoMigrate(&Instance{})
 	ctx := context{
 		dockerClient: *tmp,
+		config:       *config,
+		db:           tmp2,
 	}
-	return ctx
+	return &ctx, nil
 }
