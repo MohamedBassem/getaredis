3036cf27f027ded2ba71fb634b55890ddd411589
MohamedBassem <medoox240@gmail.com>
2015-07-07 04:44:46 +0200

    Adding monitoring hosts job

--- jobs.go
+++ jobs.go
@@ -2,7 +2,7 @@ package getaredis
 
 import "time"
 
-func CleanRedisInstaces(ctx *context) {
+func CleanRedisInstaces(ctx *context) (NumberOfContainerStopped int) {
 	var instances = make([]Instance, 0)
 	var maxTimeStamp = time.Now().Add(-1 * time.Second * 60 * 60 * time.Duration(ctx.config.MaxInstanceTime))
 	ctx.db.Model(&Instance{}).Where("running = 1 AND created_at < ?", maxTimeStamp).Find(&instances)
@@ -10,6 +10,37 @@ func CleanRedisInstaces(ctx *context) {
 	for _, instance := range instances {
 		ctx.RemoveContainer(instance.HostedAtIP, instance.ContainerID)
 	}
-
 	ctx.db.Model(&Instance{}).Where("running = 1 AND created_at < ?", maxTimeStamp).UpdateColumn("running", false)
+
+	return len(instances)
+}
+
+func MonitorHosts(ctx *context) (startedHosts, deletedHosts int, err error) {
+	hosts := ctx.ListHosts()
+	zeros := 0
+	for _, host := range hosts {
+		if host.NumberOfContainers == 0 {
+			zeros++
+		}
+	}
+	if zeros == 0 || len(hosts) == 0 {
+		err = ctx.NewHost()
+		if err != nil {
+			return -1, -1, err
+		}
+		startedHosts++
+		return
+	} else if zeros > 1 && len(hosts) > 2 {
+		for _, host := range hosts {
+			if zeros == 1 {
+				break
+			}
+			if host.NumberOfContainers == 0 {
+				err = ctx.DeleteHost(host.IP)
+				zeros--
+				deletedHosts++
+			}
+		}
+	}
+	return
 }
