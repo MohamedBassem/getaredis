18c4ba1159182d72cb794b4e54fb6327676c740a
MohamedBassem <medoox240@gmail.com>
2015-07-07 11:07:57 +0200

    Adding the webserver

---
+++ cmd/getaredis-server/public/js/main.js
@@ -0,0 +1,44 @@
+function fixLoaderPosition(){
+  var loader = document.getElementById("loader");
+  var loaderContainerWidth = loader.clientWidth;
+  var loaderWidth = parseInt(loader.children[0].clientWidth)*10;
+  loader.children[0].style.marginLeft = (loaderContainerWidth/2 - loaderWidth/2 - 10) + "px";
+}
+
+(function() {
+  fixLoaderPosition()
+  window.addEventListener("resize", fixLoaderPosition)
+  
+  var button = document.getElementById("getaredis");
+  var errorMessage = document.getElementById("error");
+  var message = document.getElementById("message");
+  button.addEventListener("click", function() {
+    button.style.display = "none";
+    errorMessage.style.display = "none";
+    message.style.display = "none";
+    loader.style.visibility = "visible";
+    var http = new XMLHttpRequest();
+    var url = "http://localhost:8080/instance";
+    http.open("POST", url, true);
+
+    http.onreadystatechange = function() { //Call a function when the state changes.
+      if(http.readyState == 4) {
+        loader.style.visibility = "hidden";
+        if(http.status == 200){
+          console.log(http.responseText);
+          data = JSON.parse(http.responseText);
+          message.innerHTML = "IP: " + data["IP"] + "<br/>Port: " + data["port"] + "<br/>Password: " + data["password"] + "<br/>";
+          message.innerHTML+= "Example:<br/>$ telnet " + data["IP"] + " " + data["port"] + "<br/>AUTH " + data["password"] +"<br/>";
+          message.innerHTML+= "PING<br/>+PONG<br/>";
+          message.style.display = "";
+        }else{
+          errorMessage.innerHTML = http.responseText;
+          errorMessage.style.display = "";
+          button.style.display = "";
+        }
+      }
+    }
+    http.send();
+  })
+
+})();
---
+++ cmd/getaredis-server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/MohamedBassem/getaredis"
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
+)
+
+func main() {
+	configFileName := flag.String("config", "", "Configuration file path")
+	port := flag.Int("port", 8080, "Server listening port")
+	flag.Parse()
+
+	if *configFileName == "" {
+		log.Fatal("A configuration file must be provided.")
+	}
+
+	ctx, err := getaredis.Init(*configFileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	m := martini.Classic()
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", "")
+	})
+
+	m.Post("/instance", func(res http.ResponseWriter, req *http.Request) (int, string) {
+		requesterIP := strings.Split(req.RemoteAddr, ":")[0]
+		err := getaredis.CheckInstanceLimit(ctx, requesterIP)
+		if err != nil {
+			return 403, err.Error()
+		}
+		instance, err := ctx.NewInstance(requesterIP)
+		if err != nil {
+			return 500, err.Error()
+		}
+		return 200, fmt.Sprintf("{\"IP\": \"%v\", \"port\": \"%v\", \"password\": \"%v\"}", instance.HostedAtIP, instance.HostedAtPort, instance.Password)
+	})
+	m.RunOnAddr(":" + strconv.Itoa(*port))
+}
