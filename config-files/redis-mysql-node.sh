87a7b6c4123acc32c9ee86a0c41718d21095ff15
MohamedBassem <medoox240@gmail.com>
2015-07-08 02:22:40 +0200

    Allowing mysql remote access

--- config-files/redis-mysql-node.sh
+++ config-files/redis-mysql-node.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-MYSQL_PASSOWRD=
+MYSQL_PASSWORD=
 REDIS_PASSWORD=
+DATABASE_NAME=getaredis
 
 #Installing MySQL
 apt-get update
-echo mysql-server mysql-server/root_password password $MYSQL_PASSOWRD | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password $MYSQL_PASSOWRD | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password password $MYSQL_PASSWORD | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD | sudo debconf-set-selections
 apt-get install -y mysql-server mysql-client
+
+#Allowing MySQL remote access
+sed -i 's/\(bind-address.*=.*\)127.0.0.1/\1 0.0.0.0/g' /etc/mysql/my.cnf
+echo "CREATE DATABASE $DATABASE_NAME; GRANT ALL ON $DATABASE_NAME.* TO root@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" > /tmp/mysqltmp
+mysql -p$MYSQL_PASSWORD < /tmp/mysqltmp
+rm /tmp/mysqltmp
+service mysql restart
 
 #Installing Redis
 add-apt-repository -y ppa:rwky/redis
